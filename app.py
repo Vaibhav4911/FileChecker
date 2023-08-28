06af3eb8529364db4e29d0de429b3d47d389b9aa
avaibhav337@gmail.com <avaibhav337@gmail.com>
2023-08-28 11:14:30 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,58 +1,52 @@
 import os
-import subprocess
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+UPLOAD_DIR = 'uploaded_files'
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_DIR'] = UPLOAD_DIR
 
 @app.route('/', methods=['GET'])
-def index():
+def main_page():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
+def upload_handler():
     uploaded_file = request.files.get('file')
     if uploaded_file:
         filename = uploaded_file.filename
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        filepath = os.path.join(app.config['UPLOAD_DIR'], filename)
         uploaded_file.save(filepath)
-        return jsonify({"message": "File uploaded successfully"})
+        return jsonify({"message": "File upload successful"})
     else:
         return jsonify({"error": "No file uploaded"})
 
-# magic_obj = magic.Magic()
-
 @app.route('/get_file_info', methods=['POST'])
-def get_file_info():
+def file_info():
     uploaded_files = request.files
     if 'file' not in uploaded_files:
         return jsonify({'error': 'No file part'})
 
     uploaded_file = uploaded_files['file']
     filename = uploaded_file.filename
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    filepath = os.path.join(app.config['UPLOAD_DIR'], filename)
     
     if os.path.exists(filepath):
-        # file_type = magic_obj.from_file(filepath)
         file_size = os.path.getsize(filepath)  # Get file size in bytes
         file_info = {
             'file_name': filename,
-            # 'file_type': file_type,
             'file_size_bytes': file_size,
             'file_size_human_readable': _format_size(file_size)
-            # You can add more information here, such as creation/modification dates, etc.
         }
         return jsonify(file_info)
     else:
         return jsonify({'error': 'File not found'})
 
 def _format_size(size):
-    # Helper function to convert bytes to a human-readable format
+    # Convert bytes to a more human-readable format
     for unit in ['B', 'KB', 'MB', 'GB']:
         if size < 1024.0:
             break
